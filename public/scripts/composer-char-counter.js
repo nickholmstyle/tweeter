74621c85c93f8ee2c63cc0ac8002e2800bda70c4
nickholmstyle <nick416holm@protonmail.com>
2022-09-17 03:02:46 +0000

    refactored my char-count and lost the red text, will have to come back to it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,41 @@
-$(document).ready(() => {
+// $(document).ready(() => {
   
-  const tweetText = $('.new-tweet')
+//   const tweetText = $('.new-tweet')
   
-  tweetText.on('input', 'textarea', function() {
-    let inputValue = $(this).val();
-    let counter = 140 - inputValue.length;
+//   tweetText.on('input', 'textarea', function() {
+//     let inputValue = $(this).val();
+//     let counter = 10 - inputValue.length;
 
-    const characterCountHtml = $(this).siblings('.tweet-footer').children('.char-counter');
-    characterCountHtml.text(140 - inputValue.length);
+//     const characterCountHtml = $(this).siblings('.tweet-footer').children('.char-counter');
+//     characterCountHtml.text(140 - inputValue.length);
 
-    if (counter >= 0) {
-      characterCountHtml.removeClass('red-text');
-    }
-    if (counter < 0) {
-      characterCountHtml.addClass('red-text');
-    }
+//     if (counter >= 0) {
+//       characterCountHtml.removeClass('red-text');
+//     }
+//     if (counter < 0) {
+//       characterCountHtml.addClass('red-text');
+//     }
 
-  });
+//   });
+// });
+
+$(() => {
+  $(".form-textarea"). on("input", onInput);
 });
+
+const onInput = function(event) {
+  
+  let $input = $(this);
+  let len = $input.val().length;
+  let charsLeft = 10 - len;
+
+  const $form = $input.closest('section');
+  const $counter = $form.find('.counter')
+
+  $counter.text(charsLeft);
+
+  if (charsLeft < 0) {
+    return $counter.addClass('.red-text');
+  }
+  $counter.removeClass('.red-text');
+}
