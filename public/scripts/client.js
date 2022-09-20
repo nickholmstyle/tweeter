7df42ed068b93dab5b387c343a4163937ae70803
nickholmstyle <nick416holm@protonmail.com>
2022-09-20 21:54:17 +0000

    XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,6 +9,14 @@
 
 $(() => {
   
+   //Prevent js from being injected in tweets from the form
+  const escape = (str) => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+
 
 //This is the tweet element
   const createTweetElement = function(tweetData) {
@@ -21,12 +29,12 @@ $(() => {
 
     let html = `
       <header class="tweet-header">
-        <img class="tweet-avatar" src="${tweetData.user.avatars}" alt="">
-        <h2 class="tweet-name">${tweetData.user.name}</h2>
-        <small class="tweet-handle">${tweetData.user.handle}</small>
+        <img class="tweet-avatar" src="${escape(tweetData.user.avatars)}" alt="">
+        <h2 class="tweet-name">${escape(tweetData.user.name)}</h2>
+        <small class="tweet-handle">${escape(tweetData.user.handle)}</small>
       </header>
         <div class="tweet-content">
-          <p>${(tweetData.content.text)}</p>
+          <p>${escape(tweetData.content.text)}</p>
         </div>
       <footer class="tweet-footer">
         <small class="footer-age">${timeago.format(tweetData.created_at)}</small>
